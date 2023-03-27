0188275379c31388cabd4a4a61a91168ed690a52
Shwetha-703 <shweta.shingare11@gmail.com>
2023-03-27 13:14:22 +0530

    FirstJSProgram

---
+++ app.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1", {id:"h1"}, "Namaste from React!!");
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(root);
+
+root.render(heading);
