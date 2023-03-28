cbc70f44659732178bfa0cf8687fc9ca42045a0b
Shwetha-703 <shweta.shingare11@gmail.com>
2023-03-28 06:03:28 +0530

    Episode 2 completed

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {id:"h1"}, "Namaste from React!!");
 
-console.log(heading);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-console.log(root);
 
 root.render(heading);
